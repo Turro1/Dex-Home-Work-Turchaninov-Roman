13f34255c61ecba57bdc322d76c21dfe9f791a8e
Turro1 <87533348+Turro1@users.noreply.github.com>
2021-07-28 00:55:37 +0300

    Add files via upload

---
+++ Home Work №3 (Упаковка и распаковка)/Home Work №3 (Упаковка и распаковка)/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace Home_Work__3__Упаковка_и_распаковка_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch stopwatchbox = new Stopwatch();
+            Stopwatch stopwatchunbox = new Stopwatch();
+            int i = 123;
+            stopwatchbox.Start();
+            object o = i; // упаковка
+            stopwatchbox.Stop();
+            stopwatchunbox.Start();
+            i = (int)o; // распаковка
+            stopwatchunbox.Stop();
+
+            Console.WriteLine($"Время упаковки: {stopwatchbox.Elapsed}\nВремя распаковки: {stopwatchunbox.Elapsed}");
+            Console.ReadLine();
+        }
+
+    }
+}

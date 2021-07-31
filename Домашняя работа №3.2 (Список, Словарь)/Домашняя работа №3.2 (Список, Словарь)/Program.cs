dccd9b5a6639bca47d907dd9354cbad34024d260
Turro1 <87533348+Turro1@users.noreply.github.com>
2021-08-01 02:54:08 +0300

    Update and rename Home Work №3.2 (List, Dictionary)/Home Work №3.2 (List, Dictionary)/Program.cs to Домашняя работа №3.2 (Список, Словарь)/Домашняя работа №3.2 (Список, Словарь)/Program.cs

---
+++ Домашняя работа №3.2 (Список, Словарь)/Домашняя работа №3.2 (Список, Словарь)/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Home_Work__3._2__List__Dictionary_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client1 = new Client() { fullName = "Sergiu", passportNumber = 5, check = 500 };
+            Client client2 = new Client() { fullName = "Roman", passportNumber = 67, check = 200 };
+            Client client3 = new Client() { fullName = "Vasiliy", passportNumber = 59, check = 50 };
+            List<Client> clients = new List<Client>();
+            clients.Add(client1);
+            clients.AddRange(new Client[] { client2, client3 });
+            foreach (Client client in clients)
+            {
+
+                int sum = clients.Sum(client.check);
+                int min = clients.Min(client.check);
+                Console.WriteLine($"Сумма всех денег в банке{sum}");
+                Console.WriteLine($"Минимальный счёт в банке{sum}");
+                client.Display();
+                Console.ReadKey();
+            }
+
+          
+                IEnumerable<string> names = from client in clients
+                                            where client.fullName.Contains("Sergiu")
+                                            select client.fullName;
+
+            foreach (string str in names)
+            {
+                Console.WriteLine($"Клиент с именем: {str}");
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
+

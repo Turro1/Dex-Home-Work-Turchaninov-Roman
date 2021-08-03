3ebd6fa5f6b9535264db326b837b46bc775665a3
Turro1 <87533348+Turro1@users.noreply.github.com>
2021-08-03 17:48:39 +0300

    Add files via upload

---
+++ Home Work №4.1 (List, Dictionary stopwatch)/Home Work №4.1 (List, Dictionary stopwatch)/Figure.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Home_Work__4._1__List__Dictionary_stopwatch_
+{
+    public class Figure
+    {
+        public int SideCount { get; set; }
+        public int SideLenght { get; set; }
+
+        public void Display()
+        {
+            Console.WriteLine($"\nКоличество сторон: {SideCount}\nДлина стороны: {SideLenght}\n");
+        }
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+            return false;
+            if (!(obj is Figure))
+            return false;
+            var result = (Figure)obj;
+            return result.SideCount == SideCount && result.SideLenght == SideLenght;
+        }
+
+        public override int GetHashCode()
+        {
+            return SideCount + SideLenght;
+        }
+    }
+}
---
+++ Home Work №4.1 (List, Dictionary stopwatch)/Home Work №4.1 (List, Dictionary stopwatch)/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace Home_Work__4._1__List__Dictionary_stopwatch_
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int count1 = 1;
+            int count2 = 1;
+            Random random = new Random();
+            Stopwatch stopWatchList = new Stopwatch();
+            Stopwatch stopWatchDict = new Stopwatch();
+            Figure figure1 = new Figure() { SideCount = 18, SideLenght = 50 };
+
+            List<Figure> figuresList = new List<Figure>();
+            
+            for (int i = 0; i < 1000; i++)
+            {
+                figuresList.Add(new Figure() { SideCount = random.Next(3, 50), SideLenght = random.Next(1, 100) });
+            }
+            stopWatchList.Start();
+            foreach (var item in figuresList)
+            {
+                figuresList.Contains(figure1);
+            }
+            stopWatchList.Stop();
+            foreach (var item in figuresList)
+            {
+                Console.WriteLine($"Фигура №{count1++}");
+                item.Display();
+            }
+
+            Dictionary<Figure, int> figuresDict = new Dictionary<Figure, int>();
+
+            for (int i = 0; i < 1000; i++)
+            {
+                if (!figuresDict.ContainsKey(figure1))
+                {
+                    figuresDict.Add(new Figure() {SideCount = random.Next(3,1000), SideLenght = random.Next(1,1000) },count2++);
+                }
+            }
+            stopWatchDict.Start();
+            foreach (var item in figuresDict) 
+            {
+               figuresDict.ContainsKey(figure1);
+            }
+            stopWatchDict.Stop();
+            foreach (var item in figuresDict)
+            {
+                Console.WriteLine($"Фигура №{item.Value}");
+                Console.WriteLine($"\nКоличество сторон: {item.Key.SideCount}\nДлина стороны: {item.Key.SideLenght}\n");
+            }
+            Console.WriteLine($"Время выполнения операции поиска для листа: {stopWatchList.ElapsedTicks / 1000}");
+            Console.WriteLine($"Время выполнения операции поиска для словаря: {stopWatchDict.ElapsedTicks / 1000}");
+            Console.ReadLine();
+
+        }
+    }
+}

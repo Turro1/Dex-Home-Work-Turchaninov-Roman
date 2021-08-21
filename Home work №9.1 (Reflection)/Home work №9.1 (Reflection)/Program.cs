9dcad7f50ce83b41d90223f1ce0fb3296be3921c
Turro1 <87533348+Turro1@users.noreply.github.com>
2021-08-21 18:49:12 +0300

    Add files via upload

---
+++ Home work №9.1 (Reflection)/Home work №9.1 (Reflection)/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+
+namespace Home_work__9._1__Reflection_
+{
+    class Program
+    {
+        public string path = Path.Combine(@"E:\\", "Курсы Dex", "Dex-Home-Work-Turchaninov-Roman", "Home work №9.1 (Reflection)", "Files");
+        static void Main(string[] args)
+        {
+            var figure = new Figure("Triangle", 3, 10);
+            FiguereProperties_1(figure);
+
+            FigureProperties_2("Home_work__9._1__Reflection_.Figure");
+            MethodFromProjectTwo(figure);
+        }
+
+        static void FiguereProperties_1(object obj)
+        {
+            var type = obj.GetType();
+            var properties = type.GetProperties(BindingFlags.Public | BindingFlags.Instance);
+
+            foreach (var property in properties)
+            {
+                Console.WriteLine($"Имя свойства = {property.Name}, Содержимое свойства = {property.GetValue(obj)}");
+            }
+        }
+
+        static void FigureProperties_2(string typeName)
+        {
+            var type = Type.GetType(typeName, false, true);
+
+            var properties = type.GetProperties(BindingFlags.Public | BindingFlags.Instance);
+
+            foreach (var property in properties)
+            {
+                Console.WriteLine($"Имя свойства = {property.Name}");
+            }
+        }
+
+        static void MethodFromProjectTwo(Figure figure)
+        {
+            string path = @"E:\Курсы Dex\Dex-Home-Work-Turchaninov-Roman\Home work №9.2 (method)\Home work №9.2 (method)\bin\Debug\netcoreapp3.1\Home work №9.2 (method).dll";
+            Assembly assembly = Assembly.LoadFrom(path);
+            var type = assembly.GetType($"Home_work__9._2__method_.Program", true, true);
+
+            object obj = Activator.CreateInstance(type);
+            MethodInfo methodInfo = type.GetMethod("TextFromFile");
+            object res = methodInfo.Invoke(obj, new object[] { $"Figure name: {figure.Name}, Side Lenght: {figure.SideLenght}" });
+            Console.Write(res.ToString());
+        }
+
+    }
+}
---
+++ Home work №9.2 (method)/Home work №9.2 (method)/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+namespace Home_work__9._2__method_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+		}
+		public static string TextFromFile(string text)
+		{
+			string path =  Path.Combine(@"E:\\","Курсы Dex","Dex-Home-Work-Turchaninov-Roman","Home work №9.1 (Reflection)","Files");
+			DirectoryInfo directoryInfo = new DirectoryInfo(path);
+
+			if (!directoryInfo.Exists)
+			{
+				directoryInfo.Create();
+			}
+			File.WriteAllText($"{path}\\properties.txt", text);
+			var prop = File.ReadAllText($"{path}\\properties.txt");
+			return $"Текст из файла -  {prop}";
+		}
+	}
+}

26bf30df2269d04874b111afd86ff01fe4b553e4
Turro1 <87533348+Turro1@users.noreply.github.com>
2021-08-22 12:58:56 +0300

    Add files via upload

---
+++ Home work №9.1 (Reflection)/Home work №9.1 (Reflection)/Figure.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Home_work__9._1__Reflection_
+{
+    public class Figure
+    {
+        public string Name { get; set; }
+        public int SideCount { get; set; }
+        public double SideLenght { get; set; }
+
+        public Figure(string Name, int SideCount, double SideLenght)
+        {
+            this.Name = Name;
+            this.SideCount = SideCount;
+            this.SideLenght = SideLenght;
+        }
+    }
+}

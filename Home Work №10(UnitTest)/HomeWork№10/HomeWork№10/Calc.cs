e502e3cb683cce0117130a722a5f33627946989c
Turro1 <87533348+Turro1@users.noreply.github.com>
2021-08-24 23:16:39 +0300

    Add files via upload

---
+++ Home Work №10(UnitTest)/HomeWork№10/HomeWork№10/Calc.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomeWork_10
+{
+    public class Calc
+    {
+        public int GetSum(int a, int b)
+        {
+            return a + b;
+
+        }
+        public int GetSubstruct(int a, int b)
+        {
+            return a - b;
+
+        }
+        public int GetMultiplication(int a, int b)
+        {
+            return a * b;
+
+        }
+        public int GetDivide(int a, int b)
+        {
+            return a / b;
+
+        }
+    }
+}
